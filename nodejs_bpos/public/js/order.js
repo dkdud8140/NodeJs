b35f22494a4c36c838fd95a557e340d94082b7fb
dkdud8140 <dkdud8140@gmail.com>
2021-09-20 23:48:09 +0900

    0920_review

--- nodejs_bpos/public/js/order.js
+++ nodejs_bpos/public/js/order.js
@@ -1,7 +1,55 @@
+const add_oreder_list = ( menu_list ) =>{
+	const order_box = document.querySelector("article.order_list");
+
+	let order_list = document.querySelectorAll("div.order_list")
+	if(order_list) {
+		order_list.forEach((order_tag)=>{
+			order_box.removeChild(order_tag);
+		})
+	}
+
+	const orders = menu_list.map( ( menu,index ) => {
+
+		order_list = document.createElement("div");
+		order_list.classList.add('order_list')
+
+		const menu_id = document.createElement("div");
+		menu_id.classList.add("menu_id");
+		menu_id.innerText = menu.p_code;
+
+		const menu_name = document.createElement("div");
+		menu_name.classList.add("menu_name");
+		menu_name.innerText = menu.p_name;
+
+		const menu_qty = document.createElement("div");
+		menu_qty.classList.add("menu_qty");
+		menu_qty.innerText = 1;
+
+		const menu_price = document.createElement("div");
+		menu_price.classList.add("menu_price");
+		menu_price.innerText = menu.p_price;
+
+		order_list.appendChild(menu_id);
+		order_list.appendChild(menu_name);
+		order_list.appendChild(menu_price);
+		order_list.appendChild(menu_qty);
+
+		return order_list;
+		// order_box.appendChild(order_list);
+	});
+	order_box.append(...orders);
+};
+
 const order_input = (table_id, menu_id) => {
+	
 	fetch(`/pos/order/${table_id}/input/${menu_id}`)
 	.then((res)=> res.json())
-	.then((result)=> console.log(result));
+	.then((result)=> {
+		console.log(result)
+		add_oreder_list(result.menu_list)
+	});
+
+
 };
 
  
@@ -15,8 +63,8 @@ document.addEventListener("DOMContentLoaded",()=>{
 	if(product_article) {
 		product_article.addEventListener("click",(e)=>{
 			const target = e.target;
-			if(target.tagName === "DIV" && 
-				target.className.includes("menu")) {
+
+			if(target.tagName === "DIV" && target.className.includes("menu")) {
 				const menu_id = target.dataset.menu_id;
 				order_input(table_id,menu_id);
 			}
--- nodejs_bpos/routes/posRouter.js
+++ nodejs_bpos/routes/posRouter.js
@@ -3,28 +3,28 @@ const router = express.Router();
 
 const { tbl_product } = require("../models/index")
 
-router.get("/order/:table_id",(req,res)=>{
+router.get("/order/:table_id", (req,res)=>{
 	const table_id = req.params.table_id;
 
-	tbl_product.findAll (
-		{ order: ["p_name", "ASC"] }
-	).then(result=>{
-		res.render("order_view", {table_id, MENU:result.row});
+	tbl_product.findAll ()
+	.then({order : ["p_name","ASC"]})
+	.then(result=>{
+		console.log("왜 언디파인디드",result)
+		res.render("order_view", {table_id, MENU:result});
 	});
 	
 }) 
 
+let menu_list = [];
 router.get("/order/:table_id/input/:menu_id",(req,res)=>{
 	const menu_id = req.params.menu_id;
 	const table_id = req.params.table_id;
 
-	const menu = { 
-		table_id,
-		menu_id,
-		menu_name: "1000원김밥",
-		menu_price: 1000,
-	};
-	res.json(menu);
+	tbl_product.findByPk(menu_id)
+	.then(result =>{
+		menu_list.push(result);
+		res.json( { table_id, menu_list })
+	})
 })
 
 module.exports = router;
